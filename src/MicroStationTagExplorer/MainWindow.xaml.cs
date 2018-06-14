cfb5b123a830bc53c8d964dc4e10246601ff58cd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-06-14 22:00:17 +0200

    Format export

--- src/MicroStationTagExplorer/MainWindow.xaml.cs
+++ src/MicroStationTagExplorer/MainWindow.xaml.cs
@@ -170,6 +170,21 @@ namespace MicroStationTagExplorer
                     Excel.Range range = ws.Range[start, end];
 
                     range.Value = values;
+
+                    ws.Rows["2:2"].Select();
+                    app.ActiveWindow.SplitColumn = 0;
+                    app.ActiveWindow.SplitRow = 1;
+                    app.ActiveWindow.FreezePanes = true;
+
+                    ws.Range[ws.Cells[1, 1], ws.Cells[1, 1]].CurrentRegion.Select();
+                    app.Selection.AutoFilter();
+
+                    ws.Columns[1].Resize(Type.Missing, 6).Select();
+                    ws.Columns[1].Resize(Type.Missing, 6).EntireColumn.AutoFit();
+
+                    ws.ListObjects.AddEx(Excel.XlListObjectSourceType.xlSrcRange, range, null, Excel.XlYesNoGuess.xlYes).Name = "Tags";
+
+                    ws.Range["A1"].Select();
                 }
                 catch (Exception ex)
                 {
