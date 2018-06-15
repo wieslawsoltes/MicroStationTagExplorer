5607f81b058c04748bf103d4efa91390e7db3a28
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-06-15 12:48:23 +0000

    Disable office interop

--- src/MicroStationTagExplorerInterop/Excelnterop.cs
+++ src/MicroStationTagExplorerInterop/Excelnterop.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using Excel = Microsoft.Office.Interop.Excel;
 
 namespace MicroStationTagExplorer
@@ -36,3 +36,4 @@ namespace MicroStationTagExplorer
         }
     }
 }
+*/
