246ec35cdfcc4e1bfa38d63270c6337df1d94207
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-06-21 06:13:59 +0000

    Update assembly info

--- src/MicroStationTagExplorer/Properties/AssemblyInfo.cs
+++ src/MicroStationTagExplorer/Properties/AssemblyInfo.cs
@@ -1,16 +1,21 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 using System.Windows;
 
 [assembly: AssemblyTitle("MicroStationTagExplorer")]
-[assembly: AssemblyDescription("MicroStation Tag Explorer")]
+[assembly: AssemblyDescription("MicroStation DGN and DWG file tag explorer.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Wiesław Šoltés")]
 [assembly: AssemblyProduct("MicroStationTagExplorer")]
 [assembly: AssemblyCopyright("Copyright © Wiesław Šoltés 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
