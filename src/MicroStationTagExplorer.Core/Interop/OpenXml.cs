371d0d615634785d91aa125a9ef00968939d5e7b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-12-13 18:57:00 +0100

    Create OpenXml.cs

---
+++ src/MicroStationTagExplorer.Core/Interop/OpenXml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace MicroStationTagExplorer.Core.Interop
+{
+    internal class OpenXml
+    {
+        public static void ExportValues(string path, object[,] values, int nRows, int nColumns, string sheetName, string tableName)
+        {
+            var spreadsheetDocument = SpreadsheetDocument.Create(path, SpreadsheetDocumentType.Workbook);
+
+            var workbookpart = spreadsheetDocument.AddWorkbookPart();
+            workbookpart.Workbook = new Workbook();
+
+            var worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+            worksheetPart.Worksheet = new Worksheet(new SheetData());
+
+            var sheets = spreadsheetDocument.WorkbookPart.Workbook.AppendChild<Sheets>(new Sheets());
+
+            Sheet sheet = new Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
+                SheetId = 1,
+                Name = sheetName
+            };
+            sheets.Append(sheet);
+
+            // TODO:
+
+            workbookpart.Workbook.Save();
+            spreadsheetDocument.Close();
+        }
+    }
+}
